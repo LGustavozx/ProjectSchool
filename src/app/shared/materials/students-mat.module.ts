0e371fce88dc0af8698bac6165e572a2faceb873
Gustavo <gustavoskt.8@hotmail.com>
2024-07-09 17:02:35 -0300

    adição do meu nav lateral

--- src/app/shared/materials/students-mat.module.ts
+++ src/app/shared/materials/students-mat.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
   exports: [
     MatIconModule,
-    MatTabsModule,
     MatButtonModule
   ],
   declarations: [],
   providers: []
 })
-export class UsersMaterialModule { }
+export class StudentsMaterialModule { }
